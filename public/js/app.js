f3c32d9d75f36a8dcc9cd43d41b52744125ca92f
Peeyush245 <peeyushpandey245@gmail.com>
2021-02-15 12:26:13 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('../../service-worker.js')
+//     .then(reg => console.log('Service worker: registered'))
+//     .catch(err => console.log(`Service worker: Error: ${err}`))
+//   })
+// }
+
+// messageOne.textContent = 'From javascript'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
+
+    fetch('/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                // console.log(data.error)
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.temperature
+                messageThree.textContent = data.humidity
+                // console.log(data.location)
+                // console.log(data.temperature)
+                // console.log(data.address)
+            }
+        })
+    })
+})
